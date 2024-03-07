5a489ccb46586f37df2f8e002eb5fd8249d59dbb
Enrico Fiorini <fiorinienr@gmail.com>
2024-03-07 19:04:11 +0100

    adding welcome log

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use serde_json::{Result, Value};
 
 mod transceiver;
 
-fn main() {
+fn main() -> () {
     // including needed config files
     let sensor_config: &'static str = include_str!("../config/sensors.json");
+    println!("==== Welcome to AltPollution! ====");
 }
